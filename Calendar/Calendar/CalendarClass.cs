17aa5a6d505a51b75de7c1142276789a29c2040c
tomkelher <tom.kelher@student.uhasselt.be>
2016-04-22 16:29:36 +0200

    Added files via upload

---
+++ Calendar/Calendar/CalendarClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System;
+
+namespace Calendar
+{
+    public class CalendarFunctions
+    {
+        string[] taken;
+
+        public CalendarFunctions()
+        {
+
+        }
+
+        public void addTaak(DateTime date, String taak)
+        {
+
+        }
+
+        public void removeTaak(DateTime date, int taakID)
+        {
+
+        }
+        public string[] getTaken(DateTime date)
+        {
+            return taken;
+        }
+
+
+
+    }
+}
---
+++ Calendar/Calendar/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Calendar
+{
+    [Activity(Label = "Calendar", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        Calendar.CalendarFunctions calendar = new Calendar.CalendarFunctions();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            CalendarView calendar = FindViewById<CalendarView>(Resource.Id.calendarView1);
+            
+            calendar.DateChange += CalendarOnDateChange;
+            //    button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+
+        private void CalendarOnDateChange(object sender, CalendarView.DateChangeEventArgs args)
+        {
+            DateTime datum = new DateTime(args.Year, args.Month, args.DayOfMonth);
+            string[] taken =  calendar.getTaken(datum);
+            ArrayAdapter adapter = new ArrayAdapter<String>(this, Resource.Layout.Main, taken);
+            ListView taskList = FindViewById<ListView>(Resource.Id.listView1);
+            taskList.Adapter = adapter;
+           
+
+        }   
+    }
+}
+
